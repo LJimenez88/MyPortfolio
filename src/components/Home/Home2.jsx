60096da394df9a0ff18d97e5ae17b73720e8a8dd
Lisandro Jimenez <jimenezlisandro88@gmail.com>
2025-05-20 09:45:30 -0700

    Updated portfolio

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
+//import Tilt from "react-parallax-tilt";
 import {
   AiOutlineTwitter,
   AiFillInstagram,
@@ -51,9 +51,9 @@ function Home2() {
             </p>
           </Col>
           <Col md={4} className="myAvtar">
-            <Tilt>
+            <div>
               <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
+            </div>
           </Col>
         </Row>
         <Row>
