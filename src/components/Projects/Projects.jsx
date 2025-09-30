0e9a89a06ac6179a5524d6ee2abb942b37f815f9
Lisandro <jimenezlisandro88@gmail.com>
2025-09-29 20:40:56 -0700

    Updated the projects site with my newest project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,7 +12,7 @@ import randomizer from "../../Assets/Projects/randomizer.png";
 import botornot from "../../Assets/Projects/botOrNot.png";
 import pennUnited from "../../Assets/Projects/pennUnited.png";
 import uccc from "../../Assets/Projects/updatedCentralCoastComedy.png";
-
+import daneCervine from "../../Assets/Projects/daneCervine.jpeg";
 
 
 
@@ -127,6 +127,16 @@ function Projects() {
               title="Updated Central Coast Comedy"
               description="A dynamic upgraded website for Central Coast Comedy that was never launched. This sort of style gives it a bit more color to it and a more modern look. It has a responsive design and event booking integrattion, making it an easy platform to use for comedy enthustiasts."
               videoLink="https://www.youtube.com/watch?v=mxAq1xdEiwU"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={daneCervine}
+              isBlog={false}
+              title="Dane Cervine - Author & Therapist"
+              description="A responsive WordPress site built to highlight books, writings, and therapy services. Beyond the custom design, I implemented PHP code to override WordPress post behavior, using category-based rules to redirect posts to external or custom pages. This ensures a seamless navigation flow while giving the client flexibility to organize and present content outside of WordPress defaults."
+              demoLink = "https://danecervine.com/"
             />
           </Col>
         </Row>
